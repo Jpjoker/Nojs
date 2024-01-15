1b0c1a6c07eaebccc91a53f1284abc17846f87af
jurgen <133093619+Jpjoker@users.noreply.github.com>
2024-01-15 23:02:06 +0100

    commentcontroller done zelfde bij de vorige commit

--- commentController.js
+++ commentController.js
@@ -2,16 +2,55 @@ const Comment = require('../models/Comment');
 const commentValidator = require('../validators/commentValidator');
 
 exports.createComment = async (req, res) => {
-   
+    try {
+        const validation = validateComment(req.body);
+        if (validation.error) {
+            return res.status(400).send(validation.error.details);
+        }
+
+        const comment = new Comment(req.body);
+        await comment.save();
+        res.status(201).send(comment);
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
 };
 
 exports.getAllComments = async (req, res) => {
-  
+    try {
+        const comments = await Comment.find();
+        res.send(comments);
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
 };
 
-exports.updateComment = async (req, res) => {   
+exports.updateComment = async (req, res) => {  
+    try {
+        const validation = validateComment(req.body);
+        if (validation.error) {
+            return res.status(400).send(validation.error.details);
+        }
+
+        const comment = await Comment.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!comment) {
+            return res.status(404).send({ message: 'Comment not found' });
+        }
+        res.send(comment);
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
 };
 
 exports.deleteComment = async (req, res) => {
+    try {
+        const comment = await Comment.findByIdAndDelete(req.params.id);
+        if (!comment) {
+            return res.status(404).send({ message: 'Comment not found' });
+        }
+        res.send({ message: 'Comment deleted' });
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
 
 };
