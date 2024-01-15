856c0058bc8e48dfaab2cda301a161c2d616ffdc
jurgen <133093619+Jpjoker@users.noreply.github.com>
2024-01-15 23:10:21 +0100

    server.js test

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const newsRoutes = require('./routes/newsRoutes');
 const commentRoutes = require('./routes/commentRoutes');
-const bodyParser = require('body-parser');
+
 const app = express();
-
-// Database configuration and connection
-const dbConfig = require('./config/db');
-mongoose.connect(dbConfig.url, dbConfig.options);
-
 app.use(bodyParser.json());
+
+// Database connection
+mongoose.connect('mongodb://localhost:27017/myapp', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('Could not connect to MongoDB', err));
 
 // Routes
 app.use('/news', newsRoutes);
