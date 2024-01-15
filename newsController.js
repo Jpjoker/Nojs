9908016ef680b20b818c396397ef9907562f2487
jurgen <133093619+Jpjoker@users.noreply.github.com>
2024-01-15 23:00:14 +0100

    newscontroller.js done maken

--- newsController.js
+++ newsController.js
@@ -2,21 +2,68 @@ const NewsPost = require('../models/NewsPost');
 const newsValidator = require('../validators/newsValidator');
 
 exports.createNewsPost = async (req, res) => {
-    // create 
+    try {
+        const validation = validateNewsPost(req.body);
+        if (validation.error) {
+            return res.status(400).send(validation.error.details);
+        }
+
+        const post = new NewsPost(req.body);
+        await post.save();
+        res.status(201).send(post);
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
 };
 
 exports.getAllNewsPosts = async (req, res) => {
-    //  get all 
+    try {
+        let { offset = 0, limit = 10 } = req.query;
+        offset = parseInt(offset);
+        limit = parseInt(limit);
+
+        const posts = await NewsPost.find().skip(offset).limit(limit);
+        res.send(posts);
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
 };
 
 exports.searchNewsPosts = async (req, res) => {
-    // search 
+    try {
+        const { title } = req.query;
+        const posts = await NewsPost.find({ title: new RegExp(title, 'i') });
+        res.send(posts);
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    } 
 };
 
 exports.updateNewsPost = async (req, res) => {
-    // update 
+    try {
+        const validation = validateNewsPost(req.body);
+        if (validation.error) {
+            return res.status(400).send(validation.error.details);
+        }
+
+        const post = await NewsPost.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!post) {
+            return res.status(404).send({ message: 'News Post not found' });
+        }
+        res.send(post);
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
 };
 
 exports.deleteNewsPost = async (req, res) => {
-    //delete
+    try {
+        const post = await NewsPost.findByIdAndDelete(req.params.id);
+        if (!post) {
+            return res.status(404).send({ message: 'News Post not found' });
+        }
+        res.send({ message: 'Post deleted' });
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
 };
