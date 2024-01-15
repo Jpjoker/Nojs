d6906c0c042bb4bc777d0532c3fb21dbca9ac656
jurgen <133093619+Jpjoker@users.noreply.github.com>
2024-01-15 22:49:04 +0100

    Controllers

---
+++ commentController.js
@@ -0,0 +1,17 @@
+const Comment = require('../models/Comment');
+const commentValidator = require('../validators/commentValidator');
+
+exports.createComment = async (req, res) => {
+   
+};
+
+exports.getAllComments = async (req, res) => {
+  
+};
+
+exports.updateComment = async (req, res) => {   
+};
+
+exports.deleteComment = async (req, res) => {
+
+};
---
+++ newsController.js
@@ -0,0 +1,22 @@
+const NewsPost = require('../models/NewsPost');
+const newsValidator = require('../validators/newsValidator');
+
+exports.createNewsPost = async (req, res) => {
+    // create 
+};
+
+exports.getAllNewsPosts = async (req, res) => {
+    //  get all 
+};
+
+exports.searchNewsPosts = async (req, res) => {
+    // search 
+};
+
+exports.updateNewsPost = async (req, res) => {
+    // update 
+};
+
+exports.deleteNewsPost = async (req, res) => {
+    //delete
+};
